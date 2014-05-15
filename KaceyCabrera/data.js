3c189fa59130a7f34321a107c4e9ec6fb812bc5c
Kacey Cabrera <kacey.cabrera@seayearup.org>
2014-05-15 16:55:30 -0700

    Create data.js

---
+++ KaceyCabrera/data.js
@@ -0,0 +1,52 @@
+[
+    {
+        "category": "Womens",
+        "Products": [
+            {
+                "name": "Sports Jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Tank Tops",
+                "price": 10,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Sweatshirts",
+                "price": 15,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Scarves",
+                "price": 14,
+                "image": "http: //someurl"
+            }
+        ]
+    },
+    {
+        "category": "Mens",
+        "Products": [
+            {
+                "name": "Sports Jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Hats",
+                "price": 15,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Sweatshirts",
+                "price": 15,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Satchels",
+                "price": 14,
+                "image": "http: //someurl"
+            }
+        ]
+    }
+]
