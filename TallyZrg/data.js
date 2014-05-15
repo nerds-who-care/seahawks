3096969aef5a234579ff712108fe33aa7328e54b
TallyZrg <ryan.wright@seayearup.org>
2014-05-15 16:59:22 -0700

    Create data.js

---
+++ TallyZrg/data.js
@@ -0,0 +1,52 @@
+var inventory=[
+    {
+        "category": "Womens",
+        "Products": [
+            {
+                "name": "Sports Jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Tank Tops",
+                "price": 10,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Beanies",
+                "price": 8,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Purses",
+                "price": 80,
+                "image": "http: //someurl"
+            }
+        ]
+    },
+    {
+        "category": "Mens",
+        "Products": [
+            {
+                "name": "Sports Jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Tank Tops",
+                "price": 10,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Beanies",
+                "price": 8,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "Satchels",
+                "price": 80,
+                "image": "http: //someurl"
+            }
+        ]
+    }
+]
