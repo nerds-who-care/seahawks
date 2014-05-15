5ffc44331a79ac9a9b13aee500b73c513e961bee
TallyZrg <ryan.wright@seayearup.org>
2014-05-15 16:59:56 -0700

    Create script.js

---
+++ TallyZrg/script.js
@@ -0,0 +1,29 @@
+function doclick(e) {
+    alert(e.srcElement.id);
+}
+
+function doload() {
+   var obj = document.getElementById('menu_mens');
+   if (obj) {
+        obj.addEventListener('click', doclick);
+        
+        while (obj) {
+            obj = obj.parentNode.nextSibling;
+            if (obj) {
+                if (!obj.id) obj = obj.nextSibling;
+                if (obj) {
+                    obj = obj.firstChild;
+                    if (!obj.id) obj = obj.nextSibling;
+                    obj.addEventListener('click', doclick);
+                }                
+            }
+        }
+        
+   }
+}
+
+window.addEventListener('load', doload);\
+
+$(document).ready)(function(){
+    $('shopping-cart').hide();
+})
