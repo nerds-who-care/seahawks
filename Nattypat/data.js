e685ec272f5890679189f83a3752fdcd706df825
Nattypat <kfcxingers@gmail.com>
2014-05-15 17:05:42 -0700

    Create data.js

---
+++ Nattypat/data.js
@@ -0,0 +1,67 @@
+var inventory = [
+    {
+        "category": "womens",
+        "products": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "tank tops",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "hats",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "purses",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "t-shirts",
+                "price": 13,
+                "image": "http: //someurl"
+            }
+        ]
+    },
+    {
+        "category": "mens",
+        "products": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "tank tops",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "hats",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "hoodies",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "t-shirts",
+                "price": 13,
+                "image": "http: //someurl"
+            },
+            {
+                "name": "satchels",
+                "price": 13,
+                "image": "http: //someurl"
+            }
+        ]
+    }
+]
