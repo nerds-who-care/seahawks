6a81a82664ec1ebcc3330895412747dfee514ffc
Joshua-Carter <joshua.carter@seayearup.org>
2014-05-15 17:04:52 -0700

    Create data.js

---
+++ joshua-carter/5-15-2014/data.js
@@ -0,0 +1,62 @@
+var inventory = [
+    {
+        "catagory": "womens",
+        "products": [
+            {
+                "name": "sports jersy",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "tank top",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "hat",
+                "price": 13,
+                "image": "http://someurl"
+            }
+        ]
+    },
+    {
+        "catagory": "mens",
+        "products": [
+            {
+                "name": "sports jersy",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "tshirt",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "hat",
+                "price": 13,
+                "image": "http://someurl"
+            }
+        ]
+    },
+    {
+        "catagory": "kids",
+        "products": [
+            {
+                "name": "sports jersy",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "toys",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "hat",
+                "price": 13,
+                "image": "http://someurl"
+            }
+        ]
+    }
+]
