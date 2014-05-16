5074f267a7cbd643ee1d402409be2400a1f3cf56
geosmile <ghernandez7529@gmail.com>
2014-05-15 17:06:57 -0700

    Create data.js

---
+++ geosmile/data.js
@@ -0,0 +1,32 @@
+var inventory=[
+    {
+        "category": "womens",
+        "products": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "tank tops",
+                "price": 10,
+                "image": "http://someurl"
+            },
+            {
+                "name": "beanies",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "purses",
+                "price": 80,
+                "image": "http://someurl"
+            },
+            {
+                "name": "t-shirts",
+                "price": 10,
+                "image": "http://someurl"
+            }
+        ]
+    }
+]
