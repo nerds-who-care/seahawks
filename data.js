80bfa5272dad6e12e1f1474f53c4655618802333
Drockio <derek.dunnom@gmail.com>
2014-05-19 14:35:28 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+var inventory = [
+    {
+        "category": "womens",
+        "products": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "tank tops",
+                "price": 10,
+                "image": "http://someurl"
+            },
+            {
+                "name": "beanies",
+                "price": 8,
+                "image": "http://someurl"
+            },
+            {
+                "name": "purses",
+                "price": 80,
+                "image": "http://someurl"
+            }
+        ]
+    },
+    {
+        "category": "mens",
+        "products": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "t-shirts",
+                "price": 10,
+                "image": "http://someurl"
+            },
+            {
+                "name": "baseball caps",
+                "price": 8,
+                "image": "http://someurl"
+            },
+            {
+                "name": "satchels",
+                "price": 20,
+                "image": "http://someurl"
+            }
+        ]
+    }
+]
