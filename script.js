1dd61a52d7fce9f9eb88bdfd1d640a381749aac4
Drockio <derek.dunnom@gmail.com>
2014-06-02 11:17:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,45 @@
-
-function doclick(e) {
-    alert(e.srcElement.id);
+function getCategoriesAndProducts(){
+    $.each(inventory, function(key, value){
+        var category = value.category;
+        var products = value.products;
+        console.log('category: ' + category);
+        $.each(products, function(key, product){
+        	var name = product.name;
+        	var price = product.price;
+        	var image = product.image;
+        	console.log('-- name: ' + name);
+        	console.log('-- price: ' + price);
+        	console.log('-- image: ' + image);
+        })
+    })
 }
 
-function test()
-{
-	$('#firstName').val('spongebob');
+function getCategories(){
+	var categories = [];
+    $.each(inventory, function(key, value){
+        var category = value.category;
+        var products = value.products;
+        console.log('category: ' + category);
+        categories.push(category);
+    })
+    return categories;
+}
+
+function displayMenu(categories){
+	var template = '<li><a id="menu_{category}" href="#">{category}</a></li>';
+	var myHtml = '';
+	$.each(categories, function(key, value){
+		myHtml += template.replace('{category}', value);
+	});
+	$('#menu').html(myHtml);
+}
+
+function displayProducts(products){
+	var template = '<li><img src="{imagePath}"><h3>Shop {category}</h3></li>';
 }
 
 $(document).ready(function(){
-
-	
-	
-
-	$('#submit').click(function(){
-		var firstName = $('#firstName').val();
-		var lastName = $('#lastName').val();
-		var address = $('#address').val();
-		var bop_it_choice = $('#bop_it_select option:selected').val();
-		var save_my_data = $('#saveData').prop('checked');
-		console.log(bop_it_choice);
-		console.log(save_my_data);
-
-		var sendData = {
-			"fName" : firstName,
-			"lName" : lastName,
-			"addr" : address,
-			"bChoice" : bop_it_choice,
-			"sData" : save_my_data
-		}
-
-		//TODO: Validate this stuff!
-
-		$.ajax({
-			url: "someUrl.aspx",
-			data: sendData,
-			type: 'POST',
-			success: function(data) {
-				alert('sweet success!');
-			},
-			error: function() {
-				alert('epic fail!');
-			}
-		})
-
-		//some other code
-	})
+	var categories = getCategories();
+	displayMenu(categories);
+	//getCategoriesAndProducts();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
