d9d45aa3f7be0b3ecb6c9783aa21cc7bcfed3b62
undeadmonkeys <walkjak14@hotmail.com>
2014-06-02 15:57:14 -0700

    Create data.js

---
+++ undeadmonkeys/data.js
@@ -0,0 +1,53 @@
+var inventory = 
+[
+    {
+        "category": "womens",
+        "product": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "tank tops",
+                "price": 10,
+                "image": "http://someurl"
+            },
+            {
+                "name": "beanies",
+                "price": 8,
+                "image": "http://someurl"
+            },
+            {
+                "name": "purses",
+                "price": 80,
+                "image": "http://someurl"
+            }
+        ]
+    },
+    {
+        "category": "mens",
+        "product": [
+            {
+                "name": "sports jersey",
+                "price": 13,
+                "image": "http://someurl"
+            },
+            {
+                "name": "t-shirts",
+                "price": 10,
+                "image": "http://someurl"
+            },
+            {
+                "name": "baseball caps",
+                "price": 8,
+                "image": "http://someurl"
+            },
+            {
+                "name": "satchels",
+                "price": 20,
+                "image": "http://someurl"
+            }
+        ]
+    }
+]
