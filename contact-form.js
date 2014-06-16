a658648f6c2382e4c1b18b924fbc240fd9ed3bb0
Stan Rawrysz <stan@scope5.com>
2014-06-16 12:40:49 -0700

    add jquery plugin for contact form

---
+++ contact-form.js
@@ -0,0 +1,15 @@
+(function ( $ ) {
+  $.fn.initializeContactForm = function() {
+    return this.each(function(){
+      // TODO put form initialization code here
+    });
+  };
+}( jQuery ));
+
+$(function(){
+  $('#contact-form').initializeContactForm();
+});
+
+
+
+
