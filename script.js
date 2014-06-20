c0c1ed8c63e3017499be5c231d3cf95c8c1e10d1
Aaron Wald <aaron.wald@docusign.com>
2014-06-19 17:03:58 -0700

    added shopping cart functionality

--- script.js
+++ script.js
@@ -82,31 +82,71 @@ function displayMenu(categories){
 
 function displayProducts(key){
   var category = inventory[key];
-  var template = '<div><img class="productimg" src="{imagePath}">{name} ${price}</div>';
+  var template = '<div>'+
+        '<img class="productimg" src="{imagePath}"> '+
+        '{name} '+
+        '${price} '+
+        '<input type="text" class="productqty" id="qty_{productid}" /> '+
+        '<button class="productbtn" id="{productid}">Add</button>'+
+    '</div>';
   var myHtml = '';
   $.each(category.products, function(key, value) {
     var temp = template.replace(/{imagePath}/g, value.image);
     temp = temp.replace(/{name}/g, value.name);
     temp = temp.replace(/{price}/g, value.price.toFixed(2));
+    temp = temp.replace(/{productid}/g, key);
     myHtml += temp;
   });
   var closex = '<div id="close-products" class="closex">X</div>';
   $('#category-products').html(myHtml + closex);
   $('#category-products').css("display","block");
   $('#close-products').on("click", function() { $('#category-products').css("display","none");});
+  $('.productbtn').on("click", function() {
+    var qty = document.getElementById("qty_" + this.id);
+    var cart = getShoppingCart();
+    var qtyval = qty.value;
+    if (isNaN(qtyval)) {
+      alert('Please enter number');
+      qty.value = "";
+      qty.focus();
+    } else {
+      cart[this.id] = qtyval;
+      setShoppingCart(cart);
+    }
+    });
   // if you take away the comments here you will get the full size image
   // when you mouse over the image in the products list
   // $('.productimg').on("mouseover", function() { this.style.width = 'auto'; });
   // $('.productimg').on("mouseout", function() { this.style.width = '80px'; });
 }
 
-$(document).ready(function(){
-  var categories = getCategories();
-  displayMenu(categories);
-  displayShoppingLinks();
+function getShoppingCart() {
+    var cart = window.localStorage.getItem('shoppingCart');
+    if (cart != undefined) {
+        return JSON.parse(cart);
+    } else {
+        var shoppingCart = [];
+        return shoppingCart;
+    }
+}
+function setShoppingCart(cart) {
+    window.localStorage.setItem('shoppingCart', JSON.stringify(cart));
+}
 
-  flickrSearch('seahawks fans');
-});
+function displayShoppingCart() {
+    var cart = getShoppingCart();
+    var template = "<div>{key} {qty}</div>";
+  var myHtml = '';
+  $.each(cart, function(key, value) {
+    var temp = template.replace(/{key}/g, key);
+    temp = temp.replace(/{qty}/g, value);
+    myHtml += temp;
+    });
+
+   $('#shoppingCart').html(myHtml);
+}
+
+
 
 
 
